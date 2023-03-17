14372d128480c0218a6ea0dc45ca7cd96ba508b8
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-17 10:55:09 +0200

    finish changes
    finish changes

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -22,6 +22,18 @@ const Section = ({ title }) => {
     setBad(state => state + 1);
   };
 
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const percentage =
+      countTotalFeedback() === 0
+        ? 0
+        : Math.floor((good / countTotalFeedback()) * 100);
+    return String(percentage) + '%';
+  };
+
   return (
     <section>
       <h1 className={css.sectionTitle}>{title}</h1>
@@ -33,119 +45,27 @@ const Section = ({ title }) => {
         labelNeutral="Neutral"
         labelBad="Bad"
       />
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        headerStatistics="Statistics"
-        headerGood="Good:"
-        headerNeutral="Neutral:"
-        headerBad="Bad:"
-      />
+      {countTotalFeedback() === 0 && (
+        <Notification header="Statistics" message="No feedback given" />
+      )}
+      {countTotalFeedback() > 0 && (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          totalFeedback={countTotalFeedback()}
+          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage()}
+          headerStatistics="Statistics"
+          headerGood="Good:"
+          headerNeutral="Neutral:"
+          headerBad="Bad:"
+          headerTotal="Total:"
+          headerPositive="Positive feedback:"
+        />
+      )}
     </section>
   );
-  //    return (
-  //     <section>
-  //        <h1 className={css.sectionTitle}>{this.props.title}</h1>
-  //        <FeedbackOptions
-  //          onGoodFeedBack={this.goodFeedBack}
-  //          onNeutralFeedBack={this.neutralFeedBack}
-  //          onBadFeedBack={this.badFeedBack}
-  //          labelGood="Good"
-  //          labelNeutral="Neutral"
-  //          labelBad="Bad"
-  //        />
-
-  //        {this.countTotalFeedback() === 0 && (
-  //          <Notification header="Statistics" message="No feedback given" />
-  //        )}
-
-  //        {this.countTotalFeedback() > 0 && (
-  //          <Statistics
-  //            good={this.state.good}
-  //            neutral={this.state.neutral}
-  //            bad={this.state.bad}
-  //            totalFeedback={this.countTotalFeedback()}
-  //            countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
-  //            headerStatistics="Statistics"
-  //            headerGood="Good:"
-  //            headerNeutral="Neutral:"
-  //            headerBad="Bad:"
-  //            headerTotal="Total:"
-  //            headerPositive="Positive feedback:"
-  //          />
-  //        )}
-  //    </section>
-  //   );
 };
-
-//class Section extends React.Component {
-//  state = {
-//    good: 0,
-//    neutral: 0,
-//    bad: 0,
-//  };
-
-//  goodFeedBack = () => {
-//    this.setState(prevState => ({ good: prevState.good + 1 }));
-//  };
-
-//  neutralFeedBack = () => {
-//    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-//  };
-
-//  badFeedBack = () => {
-//    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-//  };
-
-//  countTotalFeedback = () => {
-//    return this.state.good + this.state.neutral + this.state.bad;
-//  };
-
-//  countPositiveFeedbackPercentage = () => {
-//    const percentage =
-//      this.countTotalFeedback() === 0
-//        ? 0
-//        : Math.floor((this.state.good / this.countTotalFeedback()) * 100);
-//    return String(percentage) + '%';
-//  };
-
-//  render() {
-//    return (
-//      <section>
-//        <h1 className={css.sectionTitle}>{this.props.title}</h1>
-//        <FeedbackOptions
-//          onGoodFeedBack={this.goodFeedBack}
-//          onNeutralFeedBack={this.neutralFeedBack}
-//          onBadFeedBack={this.badFeedBack}
-//          labelGood="Good"
-//          labelNeutral="Neutral"
-//          labelBad="Bad"
-//        />
-
-//        {this.countTotalFeedback() === 0 && (
-//          <Notification header="Statistics" message="No feedback given" />
-//        )}
-
-//        {this.countTotalFeedback() > 0 && (
-//          <Statistics
-//            good={this.state.good}
-//            neutral={this.state.neutral}
-//            bad={this.state.bad}
-//            totalFeedback={this.countTotalFeedback()}
-//            countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
-//            headerStatistics="Statistics"
-//            headerGood="Good:"
-//            headerNeutral="Neutral:"
-//            headerBad="Bad:"
-//            headerTotal="Total:"
-//            headerPositive="Positive feedback:"
-//          />
-//        )}
-//      </section>
-//    );
-//  }
-//}
 
 Section.propTypes = {
   title: PropTypes.string,
